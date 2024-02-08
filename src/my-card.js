7d05c495d46dd9daf144f417c7d12d9d8ee1bf37
Alan-Manuel <abm6742@psu.edu>
2024-02-08 14:26:33 -0500

    Updated CardDeck(HunterXHunter)
    Fixed JS for changeimage and changeTitle buttons. Also, added the description(toggle) feature.

--- src/my-card.js
+++ src/my-card.js
@@ -18,28 +18,36 @@ export class MyCard extends LitElement {
     this.description ="Hello world!";
     this.buttonText= "Click me";
     this.buttonLink ="https://hunterxhunter.fandom.com/wiki/List_of_Hunter_%C3%97_Hunter_Characters";
+    this.fancy = false;
 
   }
 
   static get styles() {
     return css`
       .card {
-  width: 300px; /* Smaller card size */
+  width: 300px; 
   border: 1px solid green;
   padding: 16px;
   margin: 16px;
   box-sizing: border-box;
   background-color: #F5F5DC; 
+  height: 500px;
+}
+:host([fancy]) {
+display: block;
+  background-color: gold;
+  border: 2px solid red;
+  box-shadow: 10px 5px 5px orangered;
 }
 
 .card img {
-  width: 80%; /* Larger image size */
+  width: 80%; 
   height: 200px;
   text-align: center;
 }
 
 .card.new-background{
-  background-color: aquamarine;
+  background-color: lightgreen;
 }
 
 
@@ -64,6 +72,17 @@ export class MyCard extends LitElement {
     `;
   }
 
+
+  openChanged(e){
+    if(e.newState === "open"){
+      this.fancy = true;
+    }
+    else{
+      this.fancy = false;
+    }
+
+  }
+
   render() {
     return html`
     
@@ -71,7 +90,13 @@ export class MyCard extends LitElement {
   <div class="card">
     <img class="card-image" src="${this.imgsrc}"> 
     <h2 class="card-title"> ${this.title} </h2>
-    <p> ${this.description}</p>
+ <!-- put this in your render method where you had details -->
+ <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+  <summary>Description</summary>
+  <div>
+    <slot>${this.description}</slot>
+  </div>
+</details>
     <a href="${this.buttonLink}"> 
       <button class="card-button">${this.buttonText}</button>
     </a>
@@ -94,6 +119,7 @@ export class MyCard extends LitElement {
       description: {type: String},
       buttonText: {type: String},
       buttonLink: {type: String},
+      fancy: { type: Boolean, reflect: true },
 
       /*link:  { type: String}, body-text, text, image link, button link, button text*/
 
